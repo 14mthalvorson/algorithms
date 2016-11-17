b6ca61e0a26aa6344d1ae18975342fc194aba3fe
14mthalvorson <14mthalvorson@gmail.com>
2016-11-17 15:47:00 -0600

    Updated sorting network test.

--- sorting-network-test.py
+++ sorting-network-test.py
@@ -9,6 +9,7 @@ def is_valid_sorting_network(input_list = []):
 
     # Option for manual input if none provided
     if input_list == []:
+        print("Format:\na b")
         newline = input()
         while newline != "":
             input_list.append(tuple(newline.split(' ')))
@@ -19,19 +20,23 @@ def is_valid_sorting_network(input_list = []):
         for b in range(2):
             for c in range(2):
                 for d in range(2):
-                    for e in range(2):
-                        seq = [a, b, c, d, e] # This changes too based on bits
+                    #for e in range(2):
+                    #for f in range(2):
+                    #for g in range(2):
+                    #for h in range(2):
+                    original_seq = [a, b, c, d] # This changes too based on bits
+                    seq = original_seq
+                    # Represents the comparator
+                    for line in input_list:
+                        seq[int(line[0])] = min(seq[int(line[0])], seq[int(line[1])])
+                        seq[int(line[1])] = max(seq[int(line[0])], seq[int(line[1])])
 
-                        # Represents the comparator
-                        for line in input_list:
-                            seq[int(line[0])] = min(seq[int(line[0])], seq[int(line[1])])
-                            seq[int(line[1])] = max(seq[int(line[0])], seq[int(line[1])])
-
-                        # Sorts seq to check if network sorted the inputs correctly for this combination
-                        sorted_seq = sorted(seq)
+                    # Sorts seq to check if network sorted the inputs correctly for this combination
+                    sorted_seq = sorted(seq)
                         
-                        print(str(seq) + "  =>  " + str(seq) + " ~~~ " + str(sorted_seq == seq))
+                    print(str(original_seq) + "  =>  " + str(seq) + " ~~~ " + str(sorted_seq == seq))
 
+is_valid_sorting_network()
+# Make sure size of nested for's is correct
+# is_valid_sorting_network([(0, 1), (3, 4), (0, 2), (1, 2), (0, 3), (2, 3), (1, 4), (1, 2), (3, 4)])
 
-is_valid_sorting_network([(0, 1), (3, 4), (0, 2), (1, 2), (0, 3), (2, 3), (1, 4), (1, 2), (3, 4)])
-        
